4e632c0ea2a0ea5e8a26e86a2eae2e479a640f5d
alan tanaka <smith>
2022-10-11 17:02:09 -0300

    TDD - Starting tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+// TODO: test-driven development
+test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText("index");
   expect(linkElement).toBeInTheDocument();
-}); 
+});
